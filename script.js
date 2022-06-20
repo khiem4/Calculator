6d8d5f220475e9e0648b2d109e1a6bba2d7fb81f
khiem4 <khiemcan@gmail.com>
2022-06-20 23:59:23 +0700

    Operator be able to calculate except multiple

--- script.js
+++ script.js
@@ -1,40 +1,54 @@
 
+const display1 = document.querySelector('#display1');
+const display2 = document.querySelector('#display2')
 
-const calculator = {
-    '+': function (a, b) {
-        return a + b;
-    },
-    '-': function (a, b) {
-        return a - b;
-    },
-    '*': function (a, b) {
-        return a * b;
-    },
-    '/': function (a, b) {
-        return a / b;
-    },
-};
+const clear = document.querySelector('.clear');
+const buttons = document.querySelectorAll('.btn');
+const operators = document.querySelectorAll('.op');
+
+let value1 = 0;
+let value2 = 0;
+let operator;
+let number;
 
 
 
-function operate(list, op) {
-    return list.reduce(calculator[op])
+function operate(op, a, b) {
+    switch (op) {
+        case "+": return a + b;
+        case "-": return a - b;
+        case "*": return a * b;
+        case "/": return a / b;
+    }
 }
 
-let value;
+clear.addEventListener('click', () => {
+    display1.textContent = '';
+    display2.textContent = '';
+    value1 = 0;
+})
 
-const display = document.querySelector('#display');
-const buttons = document.querySelectorAll('.btn');
+
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        display.textContent += button.value;
-        value = parseInt(display.textContent)
+        display2.textContent += button.value;;
+        value1 = parseInt(display2.textContent);
     });
 });
 
+operators.forEach(ope => {
+    ope.addEventListener('click', () => {
+        operator = ope.value;
+        display2.textContent += operator;
+        if (operator) {
+            value2 = operate(operator, value1, value2);
+            display1.textContent = value2;
+        } else {
+            value2 = value1;
+        }
+        display2.textContent = '';
 
 
-const clear = document.querySelector('.clear');
-clear.addEventListener('click', (e) => {
-    display.textContent = '';
-})
+    });
+});
+
